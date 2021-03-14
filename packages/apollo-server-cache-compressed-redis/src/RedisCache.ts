f5496018b5a40b1895d9a2a7681b6ef77d4ad31a
Kamil Dybicz <kamil@secretescapes.com>
2021-03-14 13:13:33 +0100

    playing with concept of redis cache compression

---
+++ packages/apollo-server-cache-compressed-redis/src/RedisCache.ts
@@ -0,0 +1,73 @@
+import {
+  TestableKeyValueCache,
+  KeyValueCacheSetOptions,
+} from 'apollo-server-caching';
+import Redis, { RedisOptions } from 'ioredis';
+import DataLoader from 'dataloader';
+import snappy from 'snappy';
+
+export class RedisCache implements TestableKeyValueCache<string> {
+  readonly client: any;
+  readonly defaultSetOptions: KeyValueCacheSetOptions = {
+    ttl: 300,
+  };
+  private minimumCompressionSize = 262144;
+
+  private loader: DataLoader<string, string | null>;
+
+  constructor(options?: RedisOptions) {
+    const client = new Redis(options);
+    this.client = client;
+
+    this.loader = new DataLoader(keys => client.mget(...keys), {
+      cache: false,
+    });
+  }
+
+  async set(
+    key: string,
+    value: string,
+    options?: KeyValueCacheSetOptions,
+  ): Promise<void> {
+    const { ttl } = Object.assign({}, this.defaultSetOptions, options);
+
+    if (value.length > this.minimumCompressionSize) {
+      value = 'snappy:' + snappy.compressSync(value).toString();
+    }
+
+    if (typeof ttl === 'number') {
+      await this.client.set(key, value, 'EX', ttl);
+    } else {
+      // We'll leave out the EXpiration when no value is specified.  Of course,
+      // it may be purged from the cache for other reasons as deemed necessary.
+      await this.client.set(key, value);
+    }
+  }
+
+  async get(key: string): Promise<string | undefined> {
+    const reply = await this.loader.load(key);
+    if (reply !== null) {
+      if (reply !== undefined && reply.startsWith('snappy:')) {
+        return snappy.uncompressSync(Buffer.from(reply.slice(7)), { asBuffer: false }) as string;
+      }
+
+      return reply;
+    }
+    return;
+  }
+
+  async delete(key: string): Promise<boolean> {
+    return await this.client.del(key);
+  }
+
+  // Drops all data from Redis. This should only be used by test suites ---
+  // production code should never drop all data from an end user Redis cache!
+  async flush(): Promise<void> {
+    await this.client.flushdb();
+  }
+
+  async close(): Promise<void> {
+    await this.client.quit();
+    return;
+  }
+}
